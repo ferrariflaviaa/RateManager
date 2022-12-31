d0323b422fd807e7b58ca44b654c282abfe70dca
ferrariflaviaa <flaviaferrarit@gmail.com>
2022-12-31 15:39:19 -0300

    feat: home register

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,7 +1,45 @@
+import { useState } from 'react'
+import { Container, Form } from './register.js'
+import { Link } from 'react-router-dom'
 export default function Register() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    if(email !== '' && password !== ''){
+      alert("Teste");
+    
+    }else{
+      alert("Preencha todos os campos");
+    }
+
+  }
   return (
-    <div>
-      <h1>Página Register</h1>
-    </div>
-  );
+    <Container>
+      <h1>Cadastre-se</h1>
+      <span>Vamos criar sua conta! </span>
+
+      <Form onSubmit={handleRegister}>
+        <input
+          type="email"
+          placeholder="Digite seu email..."
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="*************"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Cadastrar</button>
+      </Form>
+      <Link to="/" className="buttonLink">
+        já possui uma conta? Faça o login!
+      </Link>
+    </Container>
+  )
 }
