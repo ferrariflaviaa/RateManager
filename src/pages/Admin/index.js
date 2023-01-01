d09cd923355087b26aaebd0f7358e9888946fd89
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-01-01 16:11:41 -0300

    feat: database fetching function

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,16 +1,48 @@
 import { useState, useEffect } from 'react'
 import { Container, Form, List } from './styles.js'
 import firebase from './../../firebaseConnection'
-import { addDoc, collection } from 'firebase/firestore'
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  query,
+  orderBy,
+  where,
+  snapshotEqual,
+} from 'firebase/firestore'
 export default function Admin() {
-  const { auth, db, firestore} = firebase;
+  const { auth, db } = firebase
   const [tarefaInput, setTarefaInput] = useState('')
   const [user, setUser] = useState('')
+  const [tarefas, setTarefas] = useState([])
 
   useEffect(() => {
     async function loadTarefas() {
       const userDatail = localStorage.getItem('@detailUser')
       setUser(JSON.parse(userDatail))
+
+      if (userDatail) {
+        const data = JSON.parse(userDatail)
+        const collectionRef = firebase
+          .firestore()
+          .collection('tarefas')
+          .where('userUid', '==', data?.uid)
+          .get()
+          .then((snapshot) => {
+            var lista = []
+
+            snapshot.forEach((doc) => {
+              lista.push({
+                id: doc.id,
+                tarefa: doc.data().tarefa,
+                userUid: doc.data().userUid,
+              })
+            })
+
+            console.log(lista)
+          })
+
+      }
     }
     loadTarefas()
   }, [])
@@ -22,18 +54,20 @@ export default function Admin() {
       alert('Digite sua tarefa...')
       return
     }
-    const collectionRef = firebase.firestore().collection('tarefas');
-    collectionRef.add({
-      tarefa: tarefaInput,
-      created: new Date(),
-      userUid: user?.uid,
-    }).then(() => {
-      console.log('Tarefa Registrada')
-      setTarefaInput('')
-    })
-    .catch((error) => {
-      console.log('ERROR AO REGISTRAR' + error)
-    })
+    const collectionRef = firebase.firestore().collection('tarefas')
+    collectionRef
+      .add({
+        tarefa: tarefaInput,
+        created: new Date(),
+        userUid: user?.uid,
+      })
+      .then(() => {
+        console.log('Tarefa Registrada')
+        setTarefaInput('')
+      })
+      .catch((error) => {
+        console.log('ERROR AO REGISTRAR' + error)
+      })
   }
 
   const handleLogout = async () => {
